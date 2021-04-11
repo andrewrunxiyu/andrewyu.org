9fc6eec9f79d44a8e90c5dd411445a5a938708f3
Andrew Yu <andrew@andrewyu.org>
2021-04-11 15:27:55 +0800

    Autogen postlist

---
+++ post/gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env/python3
+import os
+ls = os.listdir(".")
+ls.remove("gen.py")
+ls.remove("index.def.gmi")
+ls.remove("index.gmi")
+links = []
+for f in ls:
+    desc = f.replace(".gmi", "")
+    desc = desc.replace("-", " ")
+    links.append(f"=> {f} {desc}")
+yay = '\n'.join(links)
+print(yay)
+
+with open("index.def.gmi", "r") as template:
+    tplt = template.read()
+    wr = tplt.replace("[ POSTS ]", yay)
+
+with open("index.gmi", "w") as writer:
+    writer.write(wr)
+    writer.close()
